66ab7f1ddd10b5214cb1f55e2915ec1485f43cce
Sriraj Uppalapati <srirajuppalapati9@gmail.com>
2025-02-06 23:23:35 -0500

    test

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
 
       <div className="contact__container container grid">
         <div className="contact__content">
-          <h3 className="contact__title">Talk to me</h3>
+          <h3 className="contact__title">Details</h3>
 
           <div className="contact__info">
             <div className="contact__card">
@@ -39,7 +39,7 @@ const Contact = () => {
               <span className="contact__card-data">usriraj95@gmail.com</span>
 
               <a href="mailto:usriraj95@gmail.com" className="contact__button">
-                Write me{" "}
+                Write to me{" "}
               </a>
             </div>
 
